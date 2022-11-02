af12c853bc2c01a04a1d5ca3881781005d48aa1c
Sriharithaa <114979682+Sriharithaa@users.noreply.github.com>
2022-11-02 11:21:16 +0530

    Add files via upload

---
+++ IcecreamFlavour.java
@@ -0,0 +1,15 @@
+package flyweight;
+
+public class IcecreamFlavour {
+	
+
+	  private String flavourName;
+	  public IcecreamFlavour(String flavourName) {
+	    this.flavourName = flavourName;
+	  }
+	  
+	  public String toString() {
+	    return flavourName;
+	  }
+	}
+
